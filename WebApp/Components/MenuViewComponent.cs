55084640dae6774fb1a865aad6bd3bfea2d556d3
Eugene Kaler <mr.blinger@gmail.com>
2024-03-13 13:33:22 +0300

    Creating a Menu View Component

---
+++ WebApp/Components/MenuViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private List<MenuItem> _menuItems;
+        public MenuViewComponent()
+        {
+            _menuItems = new List<MenuItem> 
+            {
+                new MenuItem{ Controller="Home", Action="Index", Text="Home"},
+                new MenuItem{ Controller="Product", Action="Index", Text="Product"},
+                new MenuItem{ IsPage=true, Area="Admin", Page="/Index", Text="Admin"}
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var controller = ViewContext.RouteData.Values["controller"];
+            var page = ViewContext.RouteData.Values["page"];
+            var area = ViewContext.RouteData.Values["area"];
+
+            foreach (var item in _menuItems)
+            {
+                var _matchController = controller?.Equals(item.Controller) ?? false;
+                var _matchArea = area?.Equals(item.Area) ?? false;
+                
+                if (_matchController || _matchArea)
+                {
+                    item.Active = "active";
+                }
+            }
+
+            return View(_menuItems);
+        }
+    }
+}
