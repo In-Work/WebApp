56e127b41e144475fab4440930643d109b7dcdac
Eugene Kaler <mr.blinger@gmail.com>
2024-03-24 01:56:31 +0300

    Creating Domain Classes

---
+++ WebAppDB/Entities/Dish.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAppDB.Entities
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+        public string DishName { get; set; }
+        public string Description { get; set; }
+        public int Calories { get; set; }
+        public string Image { get; set; } 
+
+        public int DishGroupId { get; set; }
+        public DishGroup Group { get; set; }
+    }
+
+}
