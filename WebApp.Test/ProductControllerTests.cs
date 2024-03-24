9d09b28cbcefbb23cbe775ff1a92ba42b76e3fe1
Eugene Kaler <mr.blinger@gmail.com>
2024-03-24 13:53:06 +0300

    ProductController tests changed

--- WebApp.Test/ProductControllerTests.cs
+++ WebApp.Test/ProductControllerTests.cs
@@ -7,21 +7,13 @@ namespace WebApp.Test
     public class ProductControllerTests
     {
         [Theory]
-        [InlineData(1, 3, 1)]
-        [InlineData(2, 2, 4)]
+        [MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
         public void ControllerGetsProperPage(int page, int quantity, int id)
         {
             // Arrange
             var controller = new ProductController();
 
-            controller._dishes = new List<Dish>
-            {
-                new Dish{ DishId = 1},
-                new Dish{ DishId = 2},
-                new Dish{ DishId = 3},
-                new Dish{ DishId = 4},
-                new Dish{ DishId = 5}
-            };
+            controller._dishes = TestData.GetDishesList();
 
             // Act
             var result = controller.Index(page) as ViewResult;
---
+++ WebApp.Test/TestData.cs
@@ -0,0 +1,26 @@
+﻿using WebAppDB.Entities;
+
+namespace WebApp.Test
+{
+    internal static class TestData
+    {
+        public static List<Dish> GetDishesList()
+        {
+            return new List<Dish>
+            {
+                new Dish{ DishId = 1 },
+                new Dish{ DishId = 2 },
+                new Dish{ DishId = 3 },
+                new Dish{ DishId = 4 },
+                new Dish{ DishId = 5 }
+            };
+        }
+
+        public static IEnumerable<object[]> Params()
+        {
+            yield return new object[] { 1, 3, 1 };
+            yield return new object[] { 2, 2, 4 };
+        }
+    }
+
+}
