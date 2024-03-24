28ee8e426a5305b66ab4a1ed768a589d50e46e71
Eugene Kaler <mr.blinger@gmail.com>
2024-03-24 13:35:28 +0300

    Adding a Unit Test Project

---
+++ WebApp.Test/ProductControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Controllers;
+using WebAppDB.Entities;
+
+namespace WebApp.Test
+{
+    public class ProductControllerTests
+    {
+        [Theory]
+        [InlineData(1, 3, 1)]
+        [InlineData(2, 2, 4)]
+        public void ControllerGetsProperPage(int page, int quantity, int id)
+        {
+            // Arrange
+            var controller = new ProductController();
+
+            controller._dishes = new List<Dish>
+            {
+                new Dish{ DishId = 1},
+                new Dish{ DishId = 2},
+                new Dish{ DishId = 3},
+                new Dish{ DishId = 4},
+                new Dish{ DishId = 5}
+            };
+
+            // Act
+            var result = controller.Index(page) as ViewResult;
+            var model = result?.Model as List<Dish>;
+
+            // Assert
+            Assert.NotNull(model);
+            Assert.Equal(quantity, model.Count);
+            Assert.Equal(id, model[0].DishId);
+        }
+    }
+}
