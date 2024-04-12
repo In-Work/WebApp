b6ec49d7144759ed77699e819e0e1ec4dc12c9d4
Eugene Kaler <mr.blinger@gmail.com>
2024-04-12 11:11:56 +0300

    Added context tests (fixed old tests)

--- WebApp.Test/ProductControllerTests.cs
+++ WebApp.Test/ProductControllerTests.cs
@@ -1,39 +1,91 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Controllers;
 using WebAppDB.Entities;
+using WebAppDB.Data;
 using Moq;
 
 namespace WebApp.Test
 {
     public class ProductControllerTests
     {
-        //[Theory]
-        //[MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
-        //public void ControllerGetsProperPage(int page, int quantity, int id)
-        //{  
-        //    // Arrange
-        //    var controllerContext = new ControllerContext();
-        //    var moqHttpContext = new Mock<HttpContext>();
+        readonly DbContextOptions<ApplicationDbContext> _options;
+        public ProductControllerTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "testDb").Options;
+        }
 
-        //    moqHttpContext.Setup(c => c.Request.Headers).Returns(new HeaderDictionary());
-        //    controllerContext.HttpContext = moqHttpContext.Object;
+        [Theory]
+        [MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
+        public void ControllerGetsProperPage(int page, int qty, int id)
+        {
+            // Arrange
+            var controllerContext = new ControllerContext();
+            var moqHttpContext = new Mock<HttpContext>();
+            moqHttpContext.Setup(c => c.Request.Headers).Returns(new HeaderDictionary());
+            controllerContext.HttpContext = moqHttpContext.Object;
 
-        //    var controller = new ProductController()
-        //    { 
-        //        ControllerContext = controllerContext 
-        //    };
+            using (var context = new ApplicationDbContext(_options))
+            {
+                TestData.FillContext(context);
+            }
 
-        //    controller._dishes = TestData.GetDishesList();
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var controller = new ProductController(context)
+                {
+                    ControllerContext = controllerContext
+                };
 
-        //    // Act
-        //    var result = controller.Index(pageNo: page, group: null) as ViewResult;
-        //    var model = result?.Model as List<Dish>;
+                // Act
+                var result = controller.Index(group: null, pageNo: page) as ViewResult;
+                var model = result?.Model as List<Dish>;
 
-        //    // Assert
-        //    Assert.NotNull(model);
-        //    Assert.Equal(quantity, model.Count);
-        //    Assert.Equal(id, model[0].DishId);
-        //}
+                // Assert
+                Assert.NotNull(model);
+                Assert.Equal(qty, model.Count);
+                Assert.Equal(id, model[0].DishId);
+            }
+
+            using (var context = new ApplicationDbContext(_options))
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+        
+        [Fact]
+        public void ControllerSelectsGroup()
+        {
+            // Arrange
+            var controllerContext = new ControllerContext();
+            var moqHttpContext = new Mock<HttpContext>();
+            moqHttpContext.Setup(c => c.Request.Headers).Returns(new HeaderDictionary());
+            controllerContext.HttpContext = moqHttpContext.Object;
+
+            using (var context = new ApplicationDbContext(_options))
+            {
+                TestData.FillContext(context);
+            }
+
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var controller = new ProductController(context)
+                {
+                    ControllerContext = controllerContext
+                };
+
+                var comparer = Comparer<Dish>.GetComparer((d1, d2) => d1.DishId.Equals(d2.DishId));
+
+                // Act
+                var result = controller.Index(2) as ViewResult;
+                var model = result.Model as List<Dish>;
+
+                // Assert
+                Assert.Equal(2, model.Count);
+                Assert.Equal(context.Dishes.ToArrayAsync().GetAwaiter().GetResult()[2], model[0], comparer);
+            }
+        }
     }
 }
--- WebApp.Test/TestData.cs
+++ WebApp.Test/TestData.cs
@@ -1,4 +1,5 @@
-﻿using WebAppDB.Entities;
+﻿using WebAppDB.Data;
+using WebAppDB.Entities;
 
 namespace WebApp.Test
 {
@@ -21,6 +22,23 @@ namespace WebApp.Test
             yield return new object[] { 1, 3, 1 };
             yield return new object[] { 2, 2, 4 };
         }
+
+        public static void FillContext(ApplicationDbContext context)
+        {
+            context.DishGroups.Add(new DishGroup{ GroupName = "fake group" });
+
+            context.AddRange(new List<Dish>
+            {
+                new Dish{ DishId=1, DishGroupId=1},
+                new Dish{ DishId=2, DishGroupId=1},
+                new Dish{ DishId=3, DishGroupId=2},
+                new Dish{ DishId=4, DishGroupId=2},
+                new Dish{ DishId=5, DishGroupId=3}
+            });
+
+            context.SaveChanges();
+        }
+
     }
 
 }
