3c864899e13167f6dc18f43ffccb8e8b89fb9720
Eugene Kaler <mr.blinger@gmail.com>
2024-04-12 21:45:17 +0300

    Changed Cart  view component (nav-bar)

--- WebApp/Components/CartViewComponent.cs
+++ WebApp/Components/CartViewComponent.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Extensions;
+using WebApp.Models;
 
 namespace WebApp.Components
 {
     public class CartViewComponent : ViewComponent
     {
+        private Cart _cart;
+        public CartViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            return View(_cart);
         }
     }
 }
