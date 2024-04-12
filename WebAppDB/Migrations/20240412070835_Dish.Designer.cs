fcdbceae527d5a3d12b402d10c2619391e99543e
Eugene Kaler <mr.blinger@gmail.com>
2024-04-12 10:32:31 +0300

    Add migration

--- WebAppDB/Migrations/20240412070835_Dish.Designer.cs
+++ WebAppDB/Migrations/20240412070835_Dish.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppDB.Data;
 
 namespace WebAppDB.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240412070835_Dish")]
+    partial class Dish
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
