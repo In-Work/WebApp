9ba2e3cc00acb780ecf717d5a90fe3c551bd54c7
Eugene Kaler <mr.blinger@gmail.com>
2024-04-12 21:45:42 +0300

    Added Cart servises

---
+++ WebApp/Services/CartService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text.Json.Serialization;
+using WebApp.Extensions;
+using WebApp.Models;
+using WebAppDB.Entities;
+
+namespace WebApp.Services
+{
+    public class CartService : Cart
+    {
+        private readonly string sessionKey = "cart";
+
+        [JsonIgnore]
+        ISession Session { get; set; }
+
+        public static Cart GetCart(IServiceProvider sp)
+        {
+            var session = sp.GetRequiredService<IHttpContextAccessor>().HttpContext.Session;
+            var cart = session?.Get<CartService>("cart") ?? new CartService();
+            cart.Session = session;
+            return cart;
+        }
+
+        public override void AddToCart(Dish dish)
+        {
+            base.AddToCart(dish);
+            Session?.Set<CartService>(sessionKey, this);
+        }
+
+        public override void RemoveFromCart(int id)
+        {
+            base.RemoveFromCart(id);
+            Session?.Set<CartService>(sessionKey, this);
+        }
+
+        public override void ClearAll()
+        {
+            base.ClearAll();
+            Session?.Set<CartService>(sessionKey, this);
+        }
+    }
+}
